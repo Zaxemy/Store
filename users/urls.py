d314fee205be38ddaa1e6d0ee7b6f5ff5b88bfdc
[denis] <[ghghghguhggybv@gmail.com]>
2025-04-22 19:01:32 +0300

    Profile and Logout

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from users import views
-from users.views import login, registration
+from users.views import login, registration, profile
 
 
 app_name = 'users'
@@ -8,5 +8,7 @@ app_name = 'users'
 urlpatterns = [
     
     path('login/', views.login, name='login'),
-    path('registration/', views.registration, name='registration')    
+    path('registration/', views.registration, name='registration'),
+    path('profile/', views.profile, name='profile'),
+    path('logout/', views.logout, name='logout')  
 ]
--- users/views.py
+++ users/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, HttpResponseRedirect
 from users.models import User
-from users.forms import UserLoginForm, UserRegistrationForm
-from django.contrib import auth
+from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
+from django.contrib import auth, messages
 from django.urls import reverse
+
 
 # Create your views here.
 
@@ -34,3 +35,21 @@ def registration(request):
         form = UserRegistrationForm()
     context = {'form': form}
     return render(request, 'users/registration.html', context)
+
+
+def profile(request):
+    if request.method == "POST":
+        form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Поздравляю, вы успешно зарегестрировались!')
+            return HttpResponseRedirect(reverse('users:profile'))
+    else:
+        form = UserProfileForm(instance=request.user)
+    context = {'title': 'Store - Профиль', 'form': form}
+    return render(request, 'users/profile.html',context)
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
