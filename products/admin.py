96be57feafc75b26ee7552029f0073785b88f178
[denis] <[ghghghguhggybv@gmail.com]>
2025-04-27 14:24:00 +0300

    Comfortable administration panel

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,24 @@
 from django.contrib import admin
-from products.models import Category, Products
+from products.models import Category, Products, Basket
 
 #Register your models here
 
-admin.site.register(Category)
-admin.site.register(Products)
+
+@admin.register(Products)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'quantity', 'category']
+    fields = ['name', 'description', 'price', 'quantity', 'image', 'category']
+    search_fields = ['name', 'description']
+    ordering = ['name']
+    
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description']
+    fields = ['name', 'description']
+    search_fields = ['name', 'description']
+    ordering = ['name']
+    
+
+class BasketAdmin(admin.TabularInline):
+    model = Basket
+    fields = ['product', 'quantity']
